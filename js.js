aa8301b5412bacc8c21f2dfa3da88169a0983260
Georgy Byzov <sonissd@gmail.com>
2015-10-26 21:08:14 +0500

    Three View

---
+++ js.js
@@ -0,0 +1,12 @@
+		/*<script type="text/javascript">
+			$(document).ready(function(){
+				$(window).resize(function(){
+					if($(".header-links").width()<100){
+						$("li").attr({"float": "none","height": "70px"});
+					}
+					if($(window).width()>800){
+						$("#logo").attr("height", "400px");
+					}
+				});
+			});
+		</script>
